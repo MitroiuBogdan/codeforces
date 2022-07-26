025f45932fb4c8f514a5dd11f0864b5e534b17e5
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2022-07-26 23:26:55 +0300

    BeautifulMatrix.java

---
+++ src/watermalon/BeautifulMatrix.java
@@ -0,0 +1,36 @@
+package watermalon;
+
+import java.util.Scanner;
+
+public class BeautifulMatrix {
+
+    public static Scanner scanner = new Scanner(System.in);
+    public static int matix[][] = new int[5][5];
+
+    public static void main(String[] args) {
+
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < matix.length; i++) {
+            for (int j = 0; j < matix.length; j++) {
+                matix[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < matix.length; i++) {
+            for (int j = 0; j < matix.length; j++) {
+                if (matix[i][j] == 1) {
+                    y = i;
+                    x = j;
+                    break;
+                }
+            }
+        }
+//        System.out.println("x:" + x + " " + "y:" + y);
+
+        int dx = Math.abs(2 - x);
+        int dy = Math.abs(2 - y);
+        int result = dx + dy;
+        System.out.println(result);
+    }
+}
---
+++ src/watermalon/BitPlusPlus.java
@@ -0,0 +1,12 @@
+package watermalon;
+
+import java.util.Scanner;
+
+public class BitPlusPlus {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
