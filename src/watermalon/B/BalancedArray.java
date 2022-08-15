e7cbc5c712a9f439e44b9f2496a9390b27164f5f
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2022-08-15 16:00:57 +0300

    TestPooling

---
+++ src/watermalon/B/BalancedArray.java
@@ -0,0 +1,12 @@
+package watermalon.B;
+
+import java.util.Scanner;
+
+public class BalancedArray {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/watermalon/B/Drinks.java
+++ src/watermalon/B/Drinks.java
@@ -8,8 +8,23 @@ public class Drinks {
 
     public static void main(String[] args) {
 
+        int drinksPicked = scanner.nextInt();
+        double[] percentages = new double[drinksPicked];
 
-        
+        for (int i = 0; i < drinksPicked; i++) {
+            percentages[i] = scanner.nextFloat();
+        }
+
+
+        double sum = 0;
+        for (int i = 0; i < drinksPicked; i++) {
+            sum = sum + (percentages[i] / 100);
+        }
+
+        System.out.println(sum);
+        double mixedPercentage = (sum * 100) / drinksPicked;
+
+        System.out.println(mixedPercentage);
     }
 
 }
